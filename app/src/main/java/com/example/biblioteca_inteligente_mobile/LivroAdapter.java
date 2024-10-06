f564f2352a59554151582c73add43a1e50af5ff3
Mylena Santos <mylenarayane11@gmail.com>
2024-10-06 19:22:24 -0300

    Manipula os métodos do RecycleView para exibir controlar o resultado de pesquisa

---
+++ app/src/main/java/com/example/biblioteca_inteligente_mobile/LivroAdapter.java
@@ -0,0 +1,59 @@
+package com.example.biblioteca_inteligente_mobile;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LivroAdapter extends RecyclerView.Adapter<LivroAdapter.LivroViewHolder> {
+    private final ArrayList<Livro> livros;
+
+    // Construtor do adaptador
+    public LivroAdapter(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @NonNull
+    @Override
+    public LivroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Infla o layout para cada item
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_livro, parent, false);
+        return new LivroViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LivroViewHolder holder, int position) {
+        // Obtém o livro na posição atual
+        Livro livro = livros.get(position);
+        // Vincula os dados do livro à visualização
+        holder.tituloTextView.setText(livro.getTitulo());
+        holder.autorTextView.setText(livro.getAutor());
+        holder.resumoTextView.setText(livro.getResumo());
+    }
+
+    @Override
+    public int getItemCount() {
+        // Retorna o tamanho da lista de livros
+        return livros.size();
+    }
+
+    // ViewHolder para manter as referências das views
+    public static class LivroViewHolder extends RecyclerView.ViewHolder {
+        public TextView tituloTextView;
+        public TextView autorTextView;
+        public TextView resumoTextView;
+
+        public LivroViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tituloTextView = itemView.findViewById(R.id.titulo_text_view);
+            autorTextView = itemView.findViewById(R.id.autor_text_view);
+            resumoTextView = itemView.findViewById(R.id.resumo_text_view);
+        }
+    }
+}
